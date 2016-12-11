670d9913ece1be4f603bd61d6aee7c0abd364fb9
HappySaila <granthaldanewilson836@gmail.com>
2016-12-12 00:25:14 +0200

    working on fragment manager

---
+++ Assets/FragmentManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FragmentManager : MonoBehaviour {
+
+	public GameObject[] fragments;
+	GameObject Head;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
