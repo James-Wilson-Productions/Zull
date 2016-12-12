c672638577bab1130b99d742f4a6d6dd08d9c56e
James-Wilson-Productions <jameswilsonproductions@gmail.com>
2016-12-13 00:43:49 +0200

    Tutorial Base finished

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour {
+
+	void Start () {
+        MusicManager.instance.PlayMusic();
+        MusicManager.instance.Mute();
+        MusicManager.instance.TransitionSpecific(0);
+	}
+	
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player") {
+            PlayerMovement.instance.Revive();
+        }
+    }
+}
