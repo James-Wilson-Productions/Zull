9e27705855b254dbd685ca58ce4718db2cabba9b
James-Wilson-Productions <jameswilsonproductions@gmail.com>
2016-12-12 15:45:49 +0200

    Finished GUI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,21 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
+    bool settingsOpen;
+
     Animator anim;
+    Slider sfx;
+    Slider music;
+    Slider gamma;
 
     void Awake() {
         anim = GetComponent<Animator>();
+        sfx = GameObject.Find("SFX").GetComponent<Slider>();
+    }
+
+    public void startGame() {
+        Application.LoadLevel(1);
     }
 
 	public void openSettings() {
+        settingsOpen = true;
         anim.SetBool("OpenSettings", true);
-        StartCoroutine("WaitDebug");
     }
 
-    IEnumerator WaitDebug() {
-        yield return new WaitForSeconds(10);
+    public void closeSettings() {
+        settingsOpen = false;
         anim.SetBool("OpenSettings", false);
+    }
+
+    public void quit() {
+        Application.Quit();
+    }
+
+    public void sfxSlider() {
+        SoundManager.instance.Volume = sfx.value;
+    }
+
+    public void musicSlider() {
+        //TODO: Insert MusicManager.instance.Volume = to music.value here
+    }
+
+    public void gammaSlider() {
+        //TODO: Insert light brightness = to gamma.value here
     }
 }
